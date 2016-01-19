bc5c6a27ce025c95bf927face772d29083de19c7
Leibnizhu <leibnizhu@gmail.com>
2016-01-19 23:53:18 +0000

    fix some bugs

--- src/leibniz/hu/forumspider/DownloadThread.java
+++ src/leibniz/hu/forumspider/DownloadThread.java
@@ -10,34 +10,41 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
 public class DownloadThread implements Runnable {
-
+	//帖子URL
 	private String articleURL;
+	//保存的文件夹路径（已包含帖子标题的子文件夹）
+	private String dictionary;
 	
-	public DownloadThread(ArrayList<String> unHandleList) {
+	public DownloadThread(ArrayList<Map<String, String>> unHandleList, String dictionary) {
 		super();
-		this.articleURL = unHandleList.remove(0);
+		Map<String, String> tempResult = unHandleList.remove(0);
+		this.articleURL = tempResult.get("url");
+		this.dictionary = dictionary + tempResult.get("title");
 	}
 	
 	private void downImage(String imageURL){
 		//e.g. http://2342.net/1/323544ufc2de51aa6204.jpg
 		String filename = imageURL.substring(imageURL.lastIndexOf('/') + 1);
+		System.out.println("正在下载图片: " + imageURL);
 		
 		try {
 			URLConnection conn = new URL(imageURL).openConnection();
 			conn.setRequestProperty("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36");
 			InputStream inStream = conn.getInputStream();  
-			FileOutputStream fs = new FileOutputStream(new File("down", filename));  
+			FileOutputStream fs = new FileOutputStream(new File(dictionary, filename));  
 			
 			byte[] buffer = new byte[1204];  
 			int byteread = 0;
 			while ((byteread = inStream.read(buffer)) != -1) {  
 				fs.write(buffer, 0, byteread);  
-			}  
+			} 
+			fs.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}  
@@ -49,23 +56,27 @@ public class DownloadThread implements Runnable {
 		String curURL = articleURL;
 		String sTemp = articleURL.substring(0, articleURL.lastIndexOf('/'));
 		String sWebsiteLink = sTemp.substring(0, sTemp.lastIndexOf('/'));
+		
 		//图片链接的正则表达式
 		//e.g. <img src="http://23423.net/7edaa21f1d5401.jpg" alt="" />
 		Pattern pImageLink = Pattern.compile("<img src=\"(.+?jpg)\".+?/>");
 		//下一页链接的正则表达式
 		//e.g.  href="/arthtml/4sdf1-2.html" class="pagelink_a">下一页</a>
 		Pattern pNextLink = Pattern.compile("href=\"(/arthtml/.{1,20}?)\".{1,30}?>下一页</a>");
-		//没有下一页的正则表达式
-		//e.g. <a class="nolink">下一页</a>
-		//Pattern pNonextLink = Pattern.compile("<a class=\"nolink\">下一页</a>");
+		//标识是否有下一页
 		boolean nextFlag ;
 		
+		//创建保存图片的子文件夹
+		File saveDictionary = new File(dictionary);
+		if(!saveDictionary.exists()){
+			saveDictionary.mkdirs();
+		}
 		try {
 			while(true){
 				nextFlag = false;
 				URLConnection conn = new URL(curURL).openConnection();
 				conn.setRequestProperty("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36");
-				BufferedReader brWeb = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+				BufferedReader brWeb = new BufferedReader(new InputStreamReader(conn.getInputStream()), 1024*120);
 				String line = null;
 				//逐行读取返回的页面
 				while((line = brWeb.readLine()) != null) {
@@ -73,7 +84,9 @@ public class DownloadThread implements Runnable {
 					Matcher mImageLink = pImageLink.matcher(line);
 					while(mImageLink.find()){
 						//System.out.println("image: " +mImageLink.group(1));
-						downImage(mImageLink.group(1));
+						for(int i = 1; i <= mImageLink.groupCount(); i++){
+							downImage(mImageLink.group(i));
+						}
 					}
 					//匹配到下一页的链接
 					Matcher mNextLink = pNextLink.matcher(line);
--- src/leibniz/hu/forumspider/Spider.java
+++ src/leibniz/hu/forumspider/Spider.java
@@ -1,12 +1,15 @@
 package leibniz.hu.forumspider;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -22,10 +25,12 @@ import org.xml.sax.SAXException;
 
 public class Spider {
 	private String initialURL;
+	private String savepath;
 	private ArrayList<String> keywords = new ArrayList<String>();
-	private ArrayList<String> unHandleList = new ArrayList<String>();
+	private ArrayList<Map<String, String>> unHandleList = new ArrayList<Map<String, String>>();
 	
 	/**
+	 * TODO: 线程数优化
 	 * 从spider.cfg.xml文件中读取爬虫的配置
 	 */
 	private void readConfig(){
@@ -39,12 +44,13 @@ public class Spider {
 			NodeList allNodes = document.getChildNodes().item(0).getChildNodes();
 			for(int j = 0; j < allNodes.getLength(); j++){
 				Node tempNode = allNodes.item(j);
+				String tempNodeName = tempNode.getNodeName();
 				//找到初始url的节点，放入类的属性中
-				if("starturl".equals(tempNode.getNodeName())){
+				if("starturl".equals(tempNodeName)){
 					initialURL = tempNode.getTextContent();
 				}
 				//找到下载关键字的节点，将其中所有的关键字加入到类的属性中
-				if("keywords".equals(tempNode.getNodeName())){
+				if("keywords".equals(tempNodeName)){
 					NodeList keywordList = tempNode.getChildNodes();
 					for (int i = 0; i < keywordList.getLength(); i++) {
 		                Node keyword = keywordList.item(i);
@@ -53,6 +59,14 @@ public class Spider {
 		                }
 					}
 				}
+				//找到下载保存路径的节点，保存到对应属性中
+				if("savepath".equals(tempNodeName)){
+					savepath = tempNode.getTextContent();
+				}
+			}
+			File saveDictionary = new File(savepath);
+			if(!saveDictionary.exists()){
+				saveDictionary.mkdirs();
 			}
 		} catch (ParserConfigurationException e) {
 			e.printStackTrace();
@@ -60,6 +74,17 @@ public class Spider {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+	}
+	
+	private String relativeURLHandler(String relativeURL){
+		String rootURL =  initialURL.substring(0, initialURL.indexOf("/", 7));
+		String curParentURL  = initialURL.substring(0, initialURL.lastIndexOf("/"));
+		if(relativeURL.startsWith("/")){
+			//相对于网站根目录的地址
+			return rootURL + relativeURL;
+		} else {
+			return curParentURL + relativeURL;
 		}
 	}
 	
@@ -72,8 +97,6 @@ public class Spider {
 			//Regax for email address.
 			//Pattern pEmail = Pattern.compile("\\w+@\\w+(\\.\\w{1,4})+");
 			String curURL = initialURL;
-			String sTemp = initialURL.substring(0, initialURL.lastIndexOf('/'));
-			String sWebsiteLink = sTemp.substring(0, sTemp.lastIndexOf('/'));
 			//Regax for hyper links.
 			//e.g. <a href="/arthtml/233.html" target="_blank">收到分公司到法规水电费过</a>
 			Pattern pArticleLink = Pattern.compile("<a\\s*href=\"(/arthtml/.+?)\".+?>(.+?)</a>");
@@ -85,7 +108,7 @@ public class Spider {
 				//System.out.println(curURL);
 				URLConnection conn = new URL(curURL).openConnection();
 				conn.setRequestProperty("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36");
-				brWeb = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+				brWeb = new BufferedReader(new InputStreamReader(conn.getInputStream()), 1024*120);
 				String line = null;
 				//逐行读取返回的页面
 				while((line = brWeb.readLine()) != null) {
@@ -97,15 +120,19 @@ public class Spider {
 							if(mArticleLink.group(2).contains(keyword)){
 								//放入待处理队列，并跳出判断的循环
 								//如果等待序列过长，则休眠
-								while(unHandleList.size() >= 128){
+								System.out.println(unHandleList.size() );
+								while(unHandleList.size() >= 64){
 									System.out.println("Unhandled list is too long, please wait...");
-									Thread.sleep(100);
+									Thread.sleep(1000);
 								}
-								unHandleList.add(sWebsiteLink + mArticleLink.group(1));
-								//如果下载的线程太多也
-								if(Thread.activeCount() <= 128) {
-									//System.out.println("Thread stack full!");
-									new Thread(new DownloadThread(unHandleList)).start();
+								Map<String, String> tempResult = new HashMap<String, String>(); 
+								tempResult.put("title", mArticleLink.group(2));
+								tempResult.put("url", relativeURLHandler(mArticleLink.group(1)));
+								unHandleList.add(tempResult);
+								//如果下载的线程太多则等下次再开
+								if(Thread.activeCount() <= 50) {
+									System.out.println("Thread stack full!");
+									new Thread(new DownloadThread(unHandleList, savepath)).start();
 								}
 								//System.out.println(sArticleLink + mArticleLink.group(1) + "," + mArticleLink.group(2));
 								break;
@@ -115,14 +142,14 @@ public class Spider {
 					//匹配下一页链接
 					Matcher mNextLink = pNextLink.matcher(line);
 					while(mNextLink.find()){
-						curURL = sWebsiteLink + mNextLink.group(1);
+						curURL = relativeURLHandler(mNextLink.group(1));
 						break;
 					}
 				}
 				//到此读完整个页面 
 			}
-		} catch (MalformedURLException e1) {
-			e1.printStackTrace();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (InterruptedException e) {
