23892c2d59cdf0d0c23bfc3c6ae8bc4daf0cc61e
Leibniz.Hu <Leibnizhu@gmail.com>
2016-01-28 17:54:56 +0800

    Finish make web function.
    finish make web function. And split this function to a single class.

---
+++ src/leibniz/hu/forumspider/MakeWeb.java
@@ -0,0 +1,31 @@
+package leibniz.hu.forumspider;
+
+class MakeWeb{
+	
+	public static void main(String initPath){
+			List<String> imgPaths = new LinkedList<String>();
+			File curDir = new File(initPath);
+			//遍历得到文件夹下所有文件的路径+文件名(List)
+			ergodicSubDir(imgPaths, curDir);
+			//开始生成HTML
+			PrintWriter prHtml = new PrintWriter(new BufferedWriter(new FileWriter(initPath + "index.html", false)));
+			prHtml.println("<html><head><title>" + initPath + "</title></head><body align=center>");
+			for(String imgPath: imgPaths){
+				prHtml.println("<img src=\"" + imgPath + "\"/>")；
+			}
+			prHtml.println("</body></html>");
+	}
+	
+	public static void ergodicSubDir(List<String> imgPaths, File curDir){
+		Files[] subFiles = curDir.listFiles();
+		for(File subFile: subFiles){
+			if(subFile.isDictionary()){
+				//是目录，遍历之
+				ergodicSubDir(imgPaths, subFile);
+			} else if(subFile.isFile()) {
+				//是文件，添加之
+				imgPaths.add(subFile.getCanonicalPath());
+			}
+		}
+	}
+}
--- src/leibniz/hu/forumspider/SpiderUtils.java
+++ src/leibniz/hu/forumspider/SpiderUtils.java
@@ -8,28 +8,5 @@ public class SpiderUtils {
 		conn.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
 		conn.setRequestProperty("Connection", "keep-alive");
 		conn.setRequestProperty("Referer", refURL);
-	}
-	
-	public static void makeWeb(String initPath){
-			List<String> imgPaths = new LinkedList<String>();
-			File curDir = new File(initPath);
-			//遍历得到文件夹下所有文件的路径+文件名(List)
-			ergodicSubDir(imgPaths, curDir);
-			//开始生成HTML
-			BufferedWriter brHtml = new BufferedWriter(new FileWriter(initPath + "index.html", false));
-			
-	}
-	
-	public static void ergodicSubDir(List<String> imgPaths, File curDir){
-		Files[] subFiles = curDir.listFiles();
-		for(File subFile: subFiles){
-			if(subFile.isDictionary()){
-				//是目录，遍历之
-				ergodicSubDir(imgPaths, subFile);
-			} else if(subFile.isFile()) {
-				//是文件，添加之
-				imgPaths.add(subFile.getCanonicalPath());
-			}
-		}
-	}
+	}	
 }
