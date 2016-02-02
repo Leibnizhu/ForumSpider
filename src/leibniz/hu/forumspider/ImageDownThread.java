126383509369efe0f319237720e41502177fdeca
Leibniz.Hu <Leibnizhu@gmail.com>
2016-02-02 13:17:33 +0800

    Update ImageDownThread.java

--- src/leibniz/hu/forumspider/ImageDownThread.java
+++ src/leibniz/hu/forumspider/ImageDownThread.java
@@ -13,35 +13,14 @@ import java.util.Map;
 public class ImageDownThread implements Runnable {
 	private String imageURL;
 	private String saveDictionary;
-	//统计图片下载线程数
-	//static Integer threadNum= 0;
 	
-	/*
-	public ImageDownThread(String imageURL, String saveDictionary) {
-		super();
-		this.imageURL = imageURL;
-		this.saveDictionary = saveDictionary;
-	}*/
-
 	@Override
 	public void run() {
-		/*synchronized(threadNum){
-			ImageDownThread.threadNum++;
-		}*/
 		Map<String, String> tempMission = null;
-		/*if(ImageDownThread.threadNum%10 == 0){
-			System.out.println(new Date() + " 图片下载启动------------------>像我这么屌的图片下载器有" + ImageDownThread.threadNum + "个");
-		}*/
 		while(true){
-			//待办任务大于帖子解析器数量N倍则开启新线程
-			/*while((Spider.imageDownList.size() / ImageDownThread.threadNum) >= 2){
-				new Thread(new ImageDownThread(), "imageDown-" + Math.random()).start();;
-			}*/
 			if(Spider.imageDownList.size() > 0){
-				//synchronized (Spider.imageDownList) {
-					//每次从待处理队列中取出一个任务
-					tempMission = Spider.imageDownList.remove(0);
-				//}
+				//每次从待处理队列中取出一个任务
+				tempMission = Spider.imageDownList.remove(0);
 				//得到新任务的url及标题（保存路径）
 				this.imageURL = tempMission.get("imageDownURL");
 				this.saveDictionary = tempMission.get("saveDictionary");
@@ -72,9 +51,6 @@ public class ImageDownThread implements Runnable {
 					e.printStackTrace();
 					System.out.println(new Date() + " 下载图片：" + imageURL + "失败！！");
 				}  finally{
-					/*synchronized(threadNum){
-						ImageDownThread.threadNum--;
-					}*/
 					try {
 						if(fs != null){
 							fs.close();
@@ -90,19 +66,7 @@ public class ImageDownThread implements Runnable {
 			try {
 				Thread.sleep(5000);
 			} catch (InterruptedException e) {
-				//e.printStackTrace();
 			}
-			
-			/*if(Spider.imageDownList.size() / ImageDownThread.threadNum < 0.2 && ImageDownThread.threadNum > 20){
-				//待办任务小于帖子解析器数量M倍则关闭当前线程
-				break;
-			}*/
 		}
-		/*synchronized(threadNum){
-			ImageDownThread.threadNum--;
-		}
-		if(ImageDownThread.threadNum%10 == 0){
-			System.out.println(new Date() + " 图片下载器关闭-------------------------->当前运行的图片下载器有" + ImageDownThread.threadNum + "个");
-		}*/
 	}
 }
